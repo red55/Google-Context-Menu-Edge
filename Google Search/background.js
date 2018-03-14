86658d71bc33ac711e65cb60bcc7f755e295f49e
Leonid Korokh <leonid.korokh@csi-group.org>
2018-03-14 20:40:10 +0300

    add translate menu

--- Google Search/background.js
+++ Google Search/background.js
@@ -6,9 +6,24 @@ function onRequest(info, tab) {
   chrome.tabs.create({url: serviceCall});
 };
 
+function onTranslate(info, tab) {
+	var selection = info.selectionText;
+//do something with the selection
+  var serviceCall = 'https://translate.google.com/?q=' + selection;
+  chrome.tabs.create({url: serviceCall});
+};
+
 chrome.contextMenus.create({
 	id: "google_search",
 	title: "Search with Google",
 	"contexts":["selection"],
 	"onclick" : onRequest
 });
+
+chrome.contextMenus.create({
+	id: "google_translate",
+	title: "Translate with Google",
+	"contexts":["selection"],
+	"onclick" : onTranslate
+});
+
